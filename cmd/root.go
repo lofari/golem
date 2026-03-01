5949070bdf3d0d3aeea551e9a89cdb651707e2e2
Lorenzo Ricci <lorenzo.f.ricci@gmail.com>
2026-03-01 02:19:22 +0000

    feat: initialize Go module with Cobra root command

---
+++ cmd/root.go
@@ -0,0 +1,28 @@
+// cmd/root.go
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var version = "dev"
+
+var rootCmd = &cobra.Command{
+	Use:   "golem",
+	Short: "Goal-Oriented Loop Execution Manager",
+	Long:  "golem runs autonomous AI coding agent loops with persistent state across iterations.",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Version = version
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+// main.go
+package main
+
+import "github.com/winler/golem/cmd"
+
+func main() {
+	cmd.Execute()
+}
